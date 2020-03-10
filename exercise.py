559c760e5a248b462af417289555de5573e557cb
Tristan_Coutts <tristancoutt@gmail.com>
2020-03-10 15:51:31 +0200

    exercise.py

---
+++ exercise.py
@@ -0,0 +1,12 @@
+#this are numbers
+numbers=[56,78,34,21,56,34,125,45,89,75,12,56]
+print(sum(numbers))
+681
+numbers.sort()
+print(numbers)
+print(max(numbers))
+125
+print(min(numbers))
+x=list(dict. fromkeys(numbers))
+print = (x)
+
