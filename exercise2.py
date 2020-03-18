56df04f8d71852b78a5ec95c52b7ddaa03a43c29
taniantando <61458777+taniantando@users.noreply.github.com>
2020-03-18 22:40:10 +0200

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,100 @@
+def addition(x,y):
+    answer = x+y
+    return answer
+result = addition(5,7)
+print(result)
+
+
+#1
+def function(hotel_cost):
+    answer = 140
+    return answer
+result=function(140+140)
+print(result)
+
+#2
+def function(Cape_Town):
+    answer= 2500/2
+    return answer
+result=function(2500/2)
+print(result)
+
+def function(Durban):
+    answer= 2300/2
+    return answer
+result=function(2300/2)
+print(result)
+
+def function(JHB):
+    answer=2000/2
+    return answer
+result=function(2000/2)
+print(result)
+
+def function(BFN):
+    answer= 1250+1150+1000/2
+    return answer
+result=function(1250+1150+1000/2)
+print(result)
+
+
+#3
+def argument(rental_car):
+    answer=40
+    return answer
+result=argument(40+40)
+print(result)
+if result > 40:
+ print(result)
+result -= 50
+print(result)
+def arguement(rental_car):
+    answer=40
+    return answer
+result=argument(40+40)
+print(result)
+if result > 40:
+ print(result)
+result -= 20
+print(result)
+
+#4
+def function(trip_cost):
+    answer=140+2900+(-10)
+    return answer
+result=function(140+2900+(-10))
+print(result)
+
+def function(trip_cost):
+    answer=140+2900+20
+    return answer
+result=function(140+2900+20)
+print(result)
+
+#5
+def argument(spending_money):
+    answer=3060
+    return answer
+result=argument(3060)
+print(result)
+if result > 140:
+ result -= 140
+print(result)
+
+def argument(spending_money):
+    answer=3060
+    return answer
+if result > 2900:
+ result -= 2900
+print(result)
+
+def argument(spending_money):
+    answer=3060
+    return answer
+if result > 20:
+ result -= 20
+print(result)
+
+def func(*args):
+ for results in args:
+  print(result)
